2166b4139b55754aef1543986f31a986812c870d
elfiservice <elfiservice@hotmail.com>
2017-11-30 11:42:13 -0300

    feat: secao5-aula25-criando-views-dinamicas-ejs

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -14,9 +14,14 @@ module.exports = function(app) {
 
     //consultas dentro do BD
     connection.query('select * from noticias', function(error, result){
-      res.send(result);
+      //res.send(result);
+      
+      //seleciona a View q vai ser renderizado e passa o Json com o resultado para ser usado no HTML da view
+      res.render("noticias/noticias", {noticias : result});
     });
 
     // res.render("noticias/noticias");
+
+
   });
 };
