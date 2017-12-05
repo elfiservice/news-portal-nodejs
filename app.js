71ac0152dd895d333b636bb94fb2916cea9b2638
elfiservice <elfiservice@hotmail.com>
2017-12-05 07:26:15 -0300

    chore: secao7-aula30-install-consign

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ var app = require('./config/server');
 
 //apos informar ao Express q o EJS vai ter as views HTML,
 //uso o metodo Render para buscar na pasta views o arquivo .ejs com o HTML/javascript
-app.get('/tecnologia', function(req, res){
-  res.render("secao/tecnologia");
-});
+// app.get('/tecnologia', function(req, res){
+//   res.render("secao/tecnologia");
+// });
 
 //requisita o modulo HOME para buscar a view Home
-var rotaHome = require('./app/routes/home');
-//executa o modulo home em routes
-rotaHome(app);
+// var rotaHome = require('./app/routes/home');
+// //executa o modulo home em routes
+// rotaHome(app);
 
-var rotaFormInclusaoNoticias = require('./app/routes/form_inclusao_noticia')(app);
+//de uma forma de unica linha
+// var rotaFormInclusaoNoticias = require('./app/routes/form_inclusao_noticia')(app);
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+// var rotaNoticias = require('./app/routes/noticias')(app);
+
 
 
 //escutar a porta 3000 usando o APP
--- config/server.js
+++ config/server.js
@@ -3,6 +3,9 @@
 //recuprar a biblioteca do express
 var express = require('express');
 
+//instanciando o modulo Consign para add as rotas automaticamente de uma so vez
+var consign = require('consign');
+
 //executando a funcao q o modulo do express retorna acima
 var app = express();
 
@@ -11,6 +14,10 @@ app.set('view engine', 'ejs');
 
 //seta onde vai esta localizada as views a serem pegues
 app.set('views', './app/views');
+
+//incluindo todas as ROTAS dentro do nosso projeto (el busca todas as rotas dentro de app/routes)
+consign().include('app/routes').into(app);
+
 
 //exporta o app para outros arquivos js
 module.exports = app;
