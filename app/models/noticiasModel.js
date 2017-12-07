23e1a63f42c1b408874be34b67d5628a0907154a
elfiservice <elfiservice@hotmail.com>
2017-12-07 08:19:15 -0300

    feat: seca8-aula39-insert-bd

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -10,5 +10,11 @@ module.exports = function() {
     connection.query('select * from noticias WHERE id_noticias = 2', callback);
   }
 
+  this.salvarNoticia = function(noticia, connection, callback) {
+    //usando a instrução SET na string de SQL para identificar o JSON em noticia e substituier a "?"
+    //NOTA: o nome dos inputs no HTML tem q ser o mesmo do nome da coluna na Tabela do BD
+    connection.query('insert into noticias set ? ', noticia, callback);
+  }
+
   return this;
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,9 +3,20 @@ module.exports = function(app) {
     res.render("admin/form_add_noticia");
   });
 
-  //usando o metodo post que esta no EJS (HMTL) no form de envio e usando o mesmo caminho noticias/salvar 
+  //usando o metodo post que esta no EJS (HMTL) no form de envio e usando o mesmo caminho noticias/salvar
   app.post('/noticias/salvar', function(req, res){
     var noticia = req.body;
-    res.send(noticia);
+
+    //executa a funcao de conexao do BD q esta no file dbConnection.js
+    var connection = app.config.dbConnection();
+
+    //app da aplicacao o segundo app e da pasta app/models/noticiasModel
+    var noticiasModel = app.app.models.noticiasModel;
+
+    noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+      //seleciona a View q vai ser renderizado e passa o Json com o resultado para ser usado no HTML da view
+      res.redirect('/noticias');
+    });
+
   });
 };
