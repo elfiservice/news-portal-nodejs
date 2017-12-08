c0a89bcbfdccec954da3eb442b6b054d2a25c8cf
elfiservice <elfiservice@hotmail.com>
2017-12-08 07:31:36 -0300

    chore: secao10-aula43-install-express-validator-npm

--- config/server.js
+++ config/server.js
@@ -6,20 +6,24 @@ var express = require('express');
 //instanciando o modulo Consign para add as rotas automaticamente de uma so vez
 var consign = require('consign');
 
-//executando a funcao q o modulo do express retorna acima
-var app = express();
-
 //instanciando o modulo body-parser instalado usando o NPM npm install body-parser --save
 var bodyParser = require('body-parser');
 
+//instancia o modulo expressValidator
+var expressValidator = require('express-validator');
+
+//executando a funcao q o modulo do express retorna acima
+var app = express();
 //informar ao Express q o EJS vai trazer os HTMLS como views
 app.set('view engine', 'ejs');
-
 //seta onde vai esta localizada as views a serem pegues
 app.set('views', './app/views');
 
 //usando o bodyParser na aplicação com a opção extended ativa para usar json
 app.use(bodyParser.urlencoded({extended: true}));
+
+//exeutando a funcao do express-validator modulo instalado pelo npm
+app.use(expressValidator());
 
 //incluindo todas as ROTAS dentro do nosso projeto var APP, (ele busca todas as rotas dentro de app/routes)
 consign()
