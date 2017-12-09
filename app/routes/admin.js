0251022ba67217bae23f2c39de982dc392691ee7
elfiservice <elfiservice@hotmail.com>
2017-12-09 08:01:13 -0300

    feat: secao10-aula44-implem-expre-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,22 @@ module.exports = function(app) {
   app.post('/noticias/salvar', function(req, res){
     var noticia = req.body;
 
+    //usando os metodos do express-validator usando o .assert()
+    //para o titulo no primeiro param. é o name do input do HTML e no segundo parm. é a mensg d error
+    //seguido do metodo de teste .notEmpty() informand q nao pode ser vazio
+    req.assert('titulo','Título é obrigatorio').notEmpty();
+    req.assert('resumo','Resumo é obrig').notEmpty();
+    req.assert('resumo','Resumo deve ter entre 10 e 100 caracter').len(10,100);
+    req.assert('data','Data obrigatoria').notEmpty();
+    req.assert('noticia','Noticia é obrigatoria').notEmpty();
+
+    //recurpera os testes de Erros acima e vertifica se houve, se sim, retona para pagina de formulario e nao inseri.
+    var erros = req.validationErrors();
+    if(erros) {
+      res.render("admin/form_add_noticia");
+      return;
+    }
+
     //executa a funcao de conexao do BD q esta no file dbConnection.js
     var connection = app.config.dbConnection();
 
