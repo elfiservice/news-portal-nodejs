3a144e2675e3a0090a06f8b86b11de9ab47e41be
elfiservice <elfiservice@hotmail.com>
2017-12-11 06:52:37 -0300

    fix: secao10-aula45-exibindo-erros - passando variavel validacao para o ejs

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
   app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia");
+    //envia o Json validacao para ser reconhecido na View admin/form_add_noticia.ejs
+    res.render("admin/form_add_noticia", {validacao: {}});
   });
 
   //usando o metodo post que esta no EJS (HMTL) no form de envio e usando o mesmo caminho noticias/salvar
